125506e07ae3f10df8750a0f76114e69d6ee6abc
raulmori <47406865+raulmori@users.noreply.github.com>
2019-04-20 04:42:56 -0400

    Update Ex_5.vhd

--- Ex_5.vhd
+++ Ex_5.vhd
@@ -1,52 +1,49 @@
  LIBRARY ieee; 
 USE ieee.std_logic_1164.all;  
 
-entity Ex_5 is  
-  port( 
- A,B,C : in std_logic_vector(7 downto 0);
- SL1, SL2, CLK : in std_logic;
- RBX,RAX :out std_logic_vector(7 downto 0)
- );  
-end Ex_5;
+entity Circuit is  
+     port( 
+            A,B,C : in std_logic_vector(7 downto 0);              --These represent the multiple-bit "INPUTS"
+            SL1, SL2, CLK : in std_logic;                        --These represent 1-bit "INPUTS"   
+            RBX,RAX :out std_logic_vector(7 downto 0)           --These represent the multiple-bit "OUTPUTS"
+);  
+end Circuit;
 
-architecture behavioural of Ex_5 is
-signal mux_out : std_logic_vector(7 downto 0);
-signal LDA, LDB : std_logic;
-begin  
-   with SL2 select
-   mux_out <= C when '0',
-              B when '1',    
-              "00000000" when others;
+architecture behavioural of Circuit is
+
+        signal mux_out : std_logic_vector(7 downto 0);          --
+        signal DC_OUT :  std_logic_vector(1 downto 0);            --TEMPORARY output of the DECODER
+        
+        begin  
+             with SL2 select                        --Notice here that we use SELECTED-SIGNAL-ASSIGNMENT to make the MULTIPLEXER
+             mux_out <= C when '0',
+                         B when '1',    
+                         "00000000" when others;
+---------------------------------------------------------------------
+                    
+             with SL1 select                    --Here we use SELECTED-SIGNAL-ASSIGNMENT as well for the DECODER
+               DC_OUT <=
+                       "01" when '1',
+                       "10" when '0',
+                           "00" when others;         --This line is added for good practice (done to avoid bugs)
+---------- -----------------------------------------------------------
               
-decoder : process(SL1)
-           begin
-               if (SL1 = '0') then
-                   LDB <= '1';
-                   LDA <= '0';
-               elsif (SL1 = '1') then
-                   LDB <= '0';
-                   LDA <= '1';
-               else
-                   LDA <= '0';
-                   LDB <= '0';
-               end if;
-end process; 
-       
-reg1: process(CLK)
-   begin
-       if(rising_edge(CLK)) then
-           if (LDA = '1') then
-               RAX <= A;    
-           end if;
-       end if;
-   end process;
-   
- reg2: process(CLK)
-   begin
-       if(rising_edge(CLK)) then
-           if(LDB = '1') then
-               RBX <= mux_out;
-           end if;
-       end if;
-   end process;
+            reg1: process(CLK)                      --This is PROCESS for "REGISTER-A"
+              begin
+                  if(rising_edge(CLK)) then
+                      if (DC_OUT(0) = '1') then
+                          RAX <= A;    
+                      end if;
+                  end if;
+              end process;
+ ---------------------------------------------------------------------
+      
+            reg2: process(CLK)                      --This is PROCESS for "REGISTER-B"
+              begin
+                  if(rising_edge(CLK)) then
+                      if(DC_OUT(1) = '1') then
+                          RBX <= mux_out;
+                      end if;
+                  end if;
+              end process;
 end behavioural;  
